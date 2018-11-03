1c545405a5f17a082ae535e0c9f20fcd0eb7e784
Marcin <marcin.nocek.soft@gmail.com>
2018-11-03 18:07:45 +0100

    Zmieniony kod HTML na stronie.

--- bigdb/settings.py
+++ bigdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tu^ej-p-c$q+a#ksp9ppr*7-^y2=7kce0i71c+8xzc5*+-4%bv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
